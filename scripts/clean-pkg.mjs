e3da1935316089e8a978659bc09d8e6204379daf
heiwa4126 <heiwa4126@gmail.com>
2025-10-20 16:01:50 +0900

    update

--- scripts/clean-pkg.mjs
+++ scripts/clean-pkg.mjs
@@ -2,4 +2,4 @@ import fs from "node:fs";
 
 const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
 delete pkg.scripts;
-fs.writeFileSync("package.json", `${JSON.stringify(pkg, null, 2)}\n`);
+fs.writeFileSync("package.json", `${JSON.stringify(pkg, null, "\t")}\n`);
