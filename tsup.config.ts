40bf8180df32fb29ffa8084458e14d0eb9a39413
heiwa4126 <heiwa4126@gmail.com>
2025-12-03 13:56:41 +0900

    [WIP] add umd?

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,19 +2,16 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src/main.ts", "src/hello.ts"],
-	format: ["esm", "cjs"],
+	format: ["esm", "cjs", "iife"],
 	outDir: "dist",
+	minify: false,
 	bundle: false,
 	splitting: false,
 	sourcemap: true,
 	clean: true,
+	name: "Hello5",
 	dts: {
 		resolve: true,
-		entry: ["src/main.ts", "src/hello.ts"],
+		// entry: ["src/main.ts", "src/hello.ts"],
 	},
-	// outExtension({ format }) {
-	// 	return {
-	// 		js: format === "cjs" ? ".cjs" : ".js",
-	// 	};
-	// },
 });
