f158c2c62b3e098335b2750c6e86ecd880f93c5d
heiwa4126 <heiwa4126@gmail.com>
2026-01-05 13:44:58 +0900

    [WIP] fix workflow

---
+++ scripts/move-git-tag.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -uex
+TAG=$(git describe --tags --abbrev=0)
+git tag -d "${TAG}" && git push origin ":refs/tags/${TAG}"
+git add --all && git commit -am '[WIP] fix workflow'
+git tag  "${TAG}" -m ''  && git push --all --follow-tags
