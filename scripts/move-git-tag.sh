981776d7e14910ddc1940d7222585750e2ac3bda
heiwa4126 <heiwa4126@gmail.com>
2026-01-28 11:05:15 +0900

    small update. backup

--- scripts/move-git-tag.sh
+++ scripts/move-git-tag.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -uex
+#!/usr/bin/env bash
+set -euo pipefail
+
 TAG=$(git describe --tags --abbrev=0)
 git tag -d "${TAG}" && git push origin ":refs/tags/${TAG}"
 git add --all && git commit -am '[WIP] fix workflow'
--- scripts/test_server.mjs
+++ scripts/test_server.mjs
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*
 Hono を使ったシンプルなテストサーバー
 
