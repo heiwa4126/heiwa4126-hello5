3b5eac868b2f17e96636ddca5604b882ada36b64
heiwa4126 <heiwa4126@gmail.com>
2026-02-05 18:42:14 +0900

    cleanup

--- scripts/epub.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-pandoc -f gfm -t epub README.md -o README.epub \
---metadata title='npmモジュール @heiwa4126/hello5 の README' \
---metadata creator="heiwa4126@gmail.com"
